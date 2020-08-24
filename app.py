144ed1f242b6ca036073ac9f5a8b4047d51b030d
SkylerTrent <trent.skyler56@gmail.com>
2020-08-24 09:59:30 +0000

    Added get_anime def that pulls anime info from database and adds the infor to reable cards

--- app.py
+++ app.py
@@ -6,22 +6,22 @@ from bson.objectid import ObjectId
 
 app = Flask(__name__)
 app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
+app.config["MONGO_DBNAME"] = 'myTestDB'
+
 
 mongo = PyMongo(app)
 
-
 @app.route('/')
-def index():
-    return render_template("index.html")
-
-@app.route('/index.html')
-def home():
-    return render_template("index.html")
-
+@app.route('/get_anime')
+def get_anime():
+    return render_template("index.html", anime=mongo.db.myFirstMDB.find())
 @app.route('/editor.html')
 def editor_add():
     return render_template("editor.html")
-    
+
+@app.route('/index.html')
+def go_home():
+    return render_template("index.html", anime=mongo.db.myFirstMDB.find())
 
 
 if __name__ == '__main__':
