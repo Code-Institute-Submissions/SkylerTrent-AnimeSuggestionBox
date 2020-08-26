fc2230757ea53bef2c265abb16e28ecec744ae14
SkylerTrent <trent.skyler56@gmail.com>
2020-08-26 15:40:11 +0000

    Update and edit anime  into the database

--- app.py
+++ app.py
@@ -22,22 +22,35 @@ def get_anime():
 def anime_editor():
     return render_template("editor.html")
 
+
 @app.route('/')
 @app.route('/index')
 def go_home():
     return render_template("index.html", anime=mongo.db.myFirstMDB.find())
 
+
 @app.route('/edit_anime_accordian')
 def anime_or_delete():
     return render_template("edit_anime_accordian.html", anime=mongo.db.myFirstMDB.find())
 
+
 @app.route("/edit_anime/<anime_id>", methods=["GET", "POST"])
 def edit_anime(anime_id):
+    if request.method == "POST":
+        submit = {
+            "anime_name": request.form.get("anime_name"),
+            "anime_description": request.form.get("anime_description"),
+            "anime_release_date": request.form.get("anime_release_date"),
+            "anime_image": request.form.get("anime_image"),
+            "anime_url": request.form.get("anime_url")
+        }
+        mongo.db.myFirstMDB.update({"_id": ObjectId(anime_id)}, submit)
+        return redirect(url_for("get_anime"))
+
     anime = mongo.db.myFirstMDB.find_one({"_id": ObjectId(anime_id)})
     print("TESTING ANIME:", anime)
     categories = mongo.db.myFirstMDB.find().sort("anime_name", -1)
     return render_template("edit_anime.html", anime=anime, categories = categories) 
-
 
 
 @app.route('/add_anime', methods=["GET", "POST"])
--- static/js/scripts.js
+++ static/js/scripts.js
@@ -6,6 +6,7 @@
     $(document).ready(function(){
     $('.collapsible').collapsible();
     $('select').formSelect();
+      
   });
 
     (function($) {
