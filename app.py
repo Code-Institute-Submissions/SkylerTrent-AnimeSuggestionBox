6c7c5558caaaeeb841f3e6f77db60e6cc38300c3
SkylerTrent <trent.skyler56@gmail.com>
2020-08-27 11:05:27 +0000

    second code test

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app.config["MONGO_DBNAME"] = 'myTestDB'
 
 mongo = PyMongo(app)
 
-
+@app.route('/')
 @app.route('/get_anime')
 def get_anime():
     return render_template("index.html", anime=mongo.db.myFirstMDB.find())
