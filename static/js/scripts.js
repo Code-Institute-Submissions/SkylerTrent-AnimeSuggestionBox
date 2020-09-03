e79fde595fcc2625b3fdb7f49211adc5705c79ac
SkylerTrent <trent.skyler56@gmail.com>
2020-09-03 11:26:16 +0000

    Final Commit

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,21 +1,22 @@
+$(document).ready(function () {
+  $(".collapsible").collapsible();
+  $("select").formSelect();
+  $(".sidenav").sidenav();
+});
 
-    $(document).ready(function(){
-    $('.collapsible').collapsible();
-    $('select').formSelect();
-    $('.sidenav').sidenav();
-  });
-
-   
-  
-    // Floating label headings for the contact form
-    $(function() {
-      $("body").on("input propertychange", ".floating-label-form-group", function(e) {
-        $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
-      }).on("focus", ".floating-label-form-group", function() {
-        $(this).addClass("floating-label-form-group-with-focus");
-      }).on("blur", ".floating-label-form-group", function() {
-        $(this).removeClass("floating-label-form-group-with-focus");
-      });
+// Floating label headings for the contact form
+$(function () {
+  $("body")
+    .on("input propertychange", ".floating-label-form-group", function (e) {
+      $(this).toggleClass(
+        "floating-label-form-group-with-value",
+        !!$(e.target).val()
+      );
+    })
+    .on("focus", ".floating-label-form-group", function () {
+      $(this).addClass("floating-label-form-group-with-focus");
+    })
+    .on("blur", ".floating-label-form-group", function () {
+      $(this).removeClass("floating-label-form-group-with-focus");
     });
-  
- 
+});
